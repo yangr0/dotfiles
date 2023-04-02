434218bb34cefb80a5ebc9ec9b262bc06c66d30d
inc0gnit0 <iinc0gnit0@pm.me>
2023-04-01 20:34:45 -0500

    fixed installation guide

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,10 +11,11 @@ require("main")
 -- INSTALL
 -- sudo pacman -S neovim | sudo apt install neovim
 -- sudo pacman -S python | sudo apt install python3
--- curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 -- sudo pacman -S flake8 | sudo apt install flake8
 -- pip3 install --user black
+-- pip3 install --user neovim
+-- curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+-- cargo install tree-sitter-cli
 -- export PATH=$PATH:$HOME/.local/bin
 -- git clone --depth=1 https://github.com/iinc0gnit0/dotfiles
 -- cp -r dotfiles/nvim ~/.config
--- :PackerInstall
