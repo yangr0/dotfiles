ac2b44f207c1c712266614795b9d9d4bcc3ef9a2
inc0gnit0 <iinc0gnit0@pm.me>
2023-04-01 20:25:35 -0500

    added more languages to ts

--- nvim/lua/main/configs/ts.lua
+++ nvim/lua/main/configs/ts.lua
@@ -1,6 +1,9 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "lua", "vim", "vimdoc", "query", "python", "rust", "hcl" },
+  ensure_installed = { 
+    "lua", "vim", "vimdoc", "query", "python", "rust","hcl", "bash", "json",
+    "yaml", "gitignore", "markdown"
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
