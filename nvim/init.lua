1899ec61f1255393d70949400bd4ad96a9beb268
inc0gnit0 <51311097+iinc0gnit0@users.noreply.github.com>
2023-07-12 21:56:35 -0500

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,6 +12,7 @@ require("main")
 -- sudo pacman -S neovim | sudo apt install neovim
 -- sudo pacman -S python | sudo apt install python3
 -- sudo pacman -S flake8 | sudo apt install flake8
+-- sudo pacman -S python-pip | sudo apt install python3-pip
 -- pip3 install --user black
 -- pip3 install --user neovim
 -- curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
