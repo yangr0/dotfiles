4875d22571a96c76064fe5fd843f8edfc6af6c3d
inc0gnit0 <iinc0gnit0@pm.me>
2024-06-07 14:14:57 -0500

    fixed new setup

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -8,7 +8,7 @@ function run {
 }
 
 #Find out your monitor name with xrandr or arandr (save and you get this line)
-xrandr --output DP-0 --primary --mode 1920x1080 --rotate normal --rate 165.00 --output HDMI-0 --mode 1920x1080 --rotate normal --rate 144.00 --left-of DP-0
+xrandr --output DP-2 --primary --mode 2560x1440 --rotate normal --rate 165.00 --output HDMI-1 --mode 1920x1080 --rotate normal --rate 60.00 --left-of DP-2
 #xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
