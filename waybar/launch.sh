33115a961a6c98f946b0af96737dcc8f3fdbc946
raymond <ry25@illinois.edu>
2024-12-17 02:51:27 -0600

    fix temp in waybar

---
+++ waybar/launch.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# Adwaita-Dark
+
+is_swww_daemon_running() {
+    if pgrep -x "swww-daemon" > /dev/null
+    then
+        return 0
+    else
+	exec swww-daemon &
+        return 1
+    fi
+}
+
+# wait for the swww-daemon
+while ! is_swww_daemon_running; do
+    echo "wait for swww-daemon"
+    sleep 1
+done
+
+swww img ~/.config/backgrounds/minimal_landscape.jpg 
+
+pkill waybar
+
+sassc $HOME/.config/waybar/style-adwaita.scss $HOME/.config/waybar/style.css
+
+waybar
