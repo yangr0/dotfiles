37316c5aec83d229b7e8d75bc0ee03fef9af7a54
raymond <ry25@illinois.edu>
2025-02-03 17:47:23 -0600

    changed waybar font

--- .local/bin/display-on-battery.sh
+++ .local/bin/display-on-battery.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 HYPRLAND_INSTANCE_SIGNATURE=`ls /run/user/1000/hypr` su ry -c "/usr/bin/hyprctl keyword monitor ,2880x1920@60, 0x0, 1.2"
+HYPRLAND_INSTANCE_SIGNATURE=`ls /run/user/1000/hypr` su ry -c "/usr/bin/hyprctl keyword hyprctl keyword decoration:blur:enabled false"
+HYPRLAND_INSTANCE_SIGNATURE=`ls /run/user/1000/hypr` su ry -c "/usr/bin/hyprctl keyword hyprctl keyword misc:vfr true"
--- .local/bin/display-on-power.sh
+++ .local/bin/display-on-power.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 HYPRLAND_INSTANCE_SIGNATURE=`ls /run/user/1000/hypr` su ry -c "/usr/bin/hyprctl keyword monitor ,2880x1920@120, 0x0, 1.2"
+HYPRLAND_INSTANCE_SIGNATURE=`ls /run/user/1000/hypr` su ry -c "/usr/bin/hyprctl keyword hyprctl keyword decoration:blur:enabled true"
+HYPRLAND_INSTANCE_SIGNATURE=`ls /run/user/1000/hypr` su ry -c "/usr/bin/hyprctl keyword hyprctl keyword misc:vfr false"
