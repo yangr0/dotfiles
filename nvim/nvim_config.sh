4cf0e8a28dfb8a61f3ce059a8814f5bfdba78f3f
raymond <ry25@illinois.edu>
2025-05-07 17:25:00 -0500

    better lualine

--- nvim/nvim_config.sh
+++ nvim/nvim_config.sh
@@ -5,6 +5,7 @@ url=https://github.com/iinc0gnit0/dotfiles
 mkdir -p $config_dir
 git clone --depth=1 $url
 cp -r dotfiles/nvim $config_dir
+rm -rf dotfiles
 sudo apt-get install neovim python3 flake8 python3-pip || sudo pacman -S neovim python flake8 python-pip
 pip3 install --user black neovim checkov --break-system-packages
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
