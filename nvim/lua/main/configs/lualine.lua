8b2176eb70b40f7d41464a3079d866709ebc7a1a
raymond <ry25@illinois.edu>
2025-05-08 17:45:25 -0500

    better lua line and telescope

--- nvim/lua/main/configs/lualine.lua
+++ nvim/lua/main/configs/lualine.lua
@@ -12,21 +12,21 @@ require('lualine').setup {
     always_divide_middle = true,
     globalstatus = false,
     refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
+      statusline = 100,
+      tabline = 100,
+      winbar = 100,
     }
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {
+    lualine_b = {
       {
         'filename',
         newfile_status = true,
         path = 3,
       }
     },
+    lualine_c = {'branch', 'diff', 'diagnostics'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
--- nvim/lua/main/configs/telescope.lua
+++ nvim/lua/main/configs/telescope.lua
@@ -1,3 +1,8 @@
+require('telescope').setup {
+  defaults = {
+    layout_strategy = 'flex',
+  },
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
